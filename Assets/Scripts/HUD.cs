683bb7f9cc2c112b332bda6742e9f35ba54d7e10
David-Sincyr <57017527+David-Sincyr@users.noreply.github.com>
2020-03-15 11:44:02 -0400

    merged branch files
    merged health system update to new game update, works well!

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    
+    public Sprite[] HeartSprites;
+    
+    public Image HeartUI;
+
+    public GameObject player;
+    private int playerHealth;
+
+    
+
+    void Update(){
+
+        HeartUI.sprite = 
+            HeartSprites[player.GetComponent<PlayerController2D>().currentHealth];
+
+    }
+}
