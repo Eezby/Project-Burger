5f14f7bc3e7d9cb19979d745739bf3b92d4e28cb
David-Sincyr <57017527+David-Sincyr@users.noreply.github.com>
2020-04-08 19:49:39 -0400

    updated follow player and new level

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour {
 
     public float speed;
-
     private Transform target;
 
-        private void Start()
+
+    public Transform groundDetection;
+    private bool movingRight = true;
+    public float groundDistance;
+
+    private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundDistance);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                //var targetPos = new Vector2(target.position.x, transform.position.y);
+                //transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                //var targetPos = new Vector2(target.position.x, transform.position.y);
+               // transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+        //these two lines of code force the object to move across the x axis
+        //var targetPos = new Vector2(target.position.x, transform.position.y);
+        //transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        //comment both lines above and uncomment line below to add in "flying" movement
+        //transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);  
+
     }
 
 
