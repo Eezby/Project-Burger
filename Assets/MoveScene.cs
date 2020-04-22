266368fb42bc09fa766cac9122b7af6899bba426
David-Sincyr <57017527+David-Sincyr@users.noreply.github.com>
2020-04-22 19:26:00 -0400

    added/changed scripts

---
+++ Assets/MoveScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+public class MoveScene : MonoBehaviour
+{
+    [SerializeField]
+    private string Level;
+    private int nextSceneToLoad;
+
+    private void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Level);
+        }
+    }
+}
