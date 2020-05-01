defb49e280cb1f5f38352ba62d8733d0812c64c9
Michael Taylor <michael.r.taylor@maine.edu>
2020-05-01 15:30:01 -0400

    Implemented load game on start menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -14,7 +14,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !SceneManager.GetActiveScene().Equals("Menu"))
         {
             if (gamePaused)
             {
@@ -65,6 +65,7 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadGame()
     {
+        if (!pauseMenuUI.activeSelf) { PauseGame(); }
         StartCoroutine(LoadSceneRoutine());
     }
 
